26a8896469a416960dc3b672d6489cfc755242bc
Manuel <manuelmurrumeana@gmail.com>
2025-05-17 16:35:29 +0200

    test riempimento databse con faker

--- app/data/db.py
+++ app/data/db.py
@@ -18,6 +18,22 @@ def init_database() -> None:
     SQLModel.metadata.create_all(engine)
 #Questa funzione dice a SQLModel di creare tutte le tabelle nel database sulla base dei modelli (class Book(SQLModel) ecc.) che definirai.
 
+def init_database() -> None:
+    ds_exists = os.path.isfile("app/data/database.db")
+    SQLModel.metadata.create_all(engine)
+
+    if not ds_exists:
+        f = Faker("it_IT")  # istanza in italiano
+        with Session(engine) as session:
+            for _ in range(10):
+                book = Book(
+                    title=f.sentence(nb_words=5),
+                    author=f.name(),
+                    review=f.pyint(min_value=1, max_value=5)
+                )
+                session.add(book)
+            session.commit()
+
 def get_session():
     with Session(engine) as session:
         yield session
